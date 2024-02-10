c349b2ff00181432b811710059aec4686a878a08
deep-coder1 <ndktbd@gmail.com>
2024-02-10 08:22:41 +0530

    Package.json in Node JS, Ex 7 Add

---
+++ 07PackageJson.js
@@ -0,0 +1,18 @@
+console.log("Package.json");
+
+const colors = require('colors');
+const { NOTIMP } = require('dns');
+console.log("Package.json color npm Red".red);
+console.log("Package.json color npm Green".green);
+console.log("Package.json color npm bgBlue".bgBlue);
+console.log("Package.json color npm black".black);
+console.log("Package.json color npm bgYellow".bgYellow);
+
+// Note:- Node JS is a single thread not multi thread
+// Two npm install
+// npm i colors
+// npm i simple-node-logger
+
+// Suggest npm for color 
+// npm i chalk
+
