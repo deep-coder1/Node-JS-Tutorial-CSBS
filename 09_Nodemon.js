19aa4ea37b885f85c0f5420e6920b587be090d08
deep-coder1 <ndktbd@gmail.com>
2024-02-11 16:08:52 +0530

    Nodemon in Node JS, Ex 9 Add

---
+++ 09_Nodemon.js
@@ -0,0 +1,19 @@
+console.warn("Try Nodemon with node js ...");
+
+// Nodemon install 
+// npm i nodemon
+// npm install -g nodemon // Global install
+
+console.log(100+20);
+
+// nodemon 09_Nodemon.js // Run command
+
+// Interview Question: 
+// Node js is a async language
+// 1. Run First Script
+// 2. Run Second Script(complex data)
+    // --- It will not wait to finish 2nd script ---
+// 3. Run Third Script
+// 4. Continue ..
+
+// 1: 33: 04
