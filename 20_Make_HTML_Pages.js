f8bdbbc5568dd37bfdfb96d77c1c1c6f3eebd2e3
deep-coder1 <ndktbd@gmail.com>
2024-02-18 15:48:34 +0530

    Update on Make HTML Pages in Node JS, Ex 20 Add

--- 20_Make_HTML_Pages.js
+++ 20_Make_HTML_Pages.js
@@ -11,4 +11,14 @@ app.use(express.static(publicPath));
 
 app.listen(5000);
 
+// O/P:
+// http://localhost:5000/help_20.html
+// Help Page 20
+
+// http://localhost:5000/20_about.html
+// About Page 20
+
+// http://localhost:5000/
+// Home Page 20
+
 // 03:23:25 Video Time End
