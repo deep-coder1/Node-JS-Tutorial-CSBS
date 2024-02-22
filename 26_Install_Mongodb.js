af8e5be047dab1a4667f0291f2dfdceac118cb9b
deep-coder1 <ndktbd@gmail.com>
2024-02-22 18:22:36 +0530

    Install MongoDB, Shell, Compass in Node, Express JS, Ex 26 Add

--- 26_Install_Mongodb.js
+++ 26_Install_Mongodb.js
@@ -6,3 +6,14 @@
 // Mongodb compass Tool
 // Interview Question
 
+// Install MongoDB 7,
+// MongoDB Shell
+// MongoDB Compass (GUI)
+
+// mongod --version
+// mongosh
+// show dbs
+// use deepakdb
+// db.createCollection("node-tut")
+
+// 04:21:3 Video Time End
