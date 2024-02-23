f34b8bf1a17dbaeb999109052b321663107497d3
deep-coder1 <ndktbd@gmail.com>
2024-02-23 18:25:45 +0530

    MongoDB Basic in Node, Express JS, Ex 27 Add, MongoDB

---
+++ 27_MongoDB_Basic.js
@@ -0,0 +1,68 @@
+// MongoDB Basic
+
+// What is MongoDB
+// MongoDB vs SQL
+// MongoDB basic commands
+// Interview Question
+
+// --> What is MongoDB
+// MongoDB is a NoSQL database.
+// The data stored in a collection.
+// Collection don't have row and columns.
+// Data is stored in the form of object.
+
+// MongoDB Shell command create database and collections and delete database and collections.
+
+// show dbs
+// admin      40.00 KiB
+// config    108.00 KiB
+// deepakdb    8.00 KiB
+// local      40.00 KiB
+// use youtubedb
+// switched to db youtubedb
+// db.createCollection("videos")
+// { ok: 1 }
+// show dbs
+// admin       40.00 KiB
+// config     108.00 KiB
+// deepakdb     8.00 KiB
+// local       40.00 KiB
+// youtubedb    8.00 KiB
+// db.createCollection("videos")
+// { ok: 1 }
+// show dbs
+// admin       40.00 KiB
+// config     108.00 KiB
+// deepakdb     8.00 KiB
+// local       40.00 KiB
+// youtubedb    8.00 KiB
+// show collections
+// videos
+// db.createCollection("playlists")
+// { ok: 1 }
+// db.playlists.drop()
+// true
+// show collections
+// videos
+// show dbs
+// admin       40.00 KiB
+// config     108.00 KiB
+// deepakdb     8.00 KiB
+// local       40.00 KiB
+// youtubedb    8.00 KiB
+// db.dropDatabase()
+// { ok: 1, dropped: 'youtubedb' }
+// db
+// youtubedb
+// show dbs
+// admin      40.00 KiB
+// config    108.00 KiB
+// deepakdb    8.00 KiB
+// local      40.00 KiB
+// use deepakdb
+// switched to db deepakdb
+// db
+// deepakdb
+
+
+// 04:36:00 Video Time End
