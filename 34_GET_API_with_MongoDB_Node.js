aaf0a39b7c091a1a34eeee0a4cf548ebbde4e3a5
deep-coder1 <ndktbd@gmail.com>
2024-02-26 17:47:42 +0530

    GET API with MongoDB and  node js to node js, Express JS, Ex 33 Add

---
+++ 34_GET_API_with_MongoDB_Node.js
@@ -0,0 +1,24 @@
+// GET API with MongoDB and  node js
+
+// API with MongoDB
+// Make new file for API
+// Import and use Mongo Config
+// Make API for get data
+// Test with Postman
+// Interview Question
+
+const express = require('express');
+const dbConnect = require('./mongodb');
+const app = express();
+
+app.get('/', async (req,resp)=>{
+    let data = await dbConnect();
+    data = await data.find().toArray();
+    console.log(data);
+    // resp.send({name:'deepak'})
+    resp.send(data);
+});
+
+app.listen(5000);
+
+// 05:54:07 Video Time End
