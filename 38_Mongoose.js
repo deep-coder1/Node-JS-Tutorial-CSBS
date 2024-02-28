c2aadaf97b2a3d553901357580ec5c13199ab041
deep-coder1 <ndktbd@gmail.com>
2024-02-28 16:23:06 +0530

    Mongoose in Node JS, Express JS, Ex 38 Add

---
+++ 38_Mongoose.js
@@ -0,0 +1,26 @@
+// Start with Mongoose V - 38
+
+// What is Mongoose
+// Difference between Mongoose vs MongoDB Package
+// Install Mongoose
+// What is schemas
+// What is Model
+// Connect Node js and MongoDB with Mongose
+
+const mongoose = require('mongoose');
+
+const main = async () =>{
+    await mongoose.connect("mongodb://localhost:27017/e-comm");
+    const ProductSchema = new mongoose.Schema({
+        name:String,
+        price:Number
+    });
+    const ProductsModel = mongoose.model('products', ProductSchema);
+    let data = new ProductsModel({name:"m10", price:800});
+    let result = await data.save();
+    console.log(result);
+}
+
+main();
+
+// 06:24:17 Video Time End
