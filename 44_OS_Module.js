3a279cd99f653c7f165147ff9d78ac5f9f355742
deep-coder1 <ndktbd@gmail.com>
2024-03-03 09:12:16 +0530

    OS Module in Node JS, Express JS Ex 44 Add

---
+++ 44_OS_Module.js
@@ -0,0 +1,17 @@
+// OS Module in Node JS Vid - 44
+
+// What is OS Module
+// Important functions of OS module
+// Interview Question
+
+const os = require('os');
+// console.log("OS: ",os);
+// console.log("OS Arch: ",os.arch());
+// console.log("OS Free Memory: ",os.freemem());
+// console.log("OS Free Memory: ",os.freemem()/(1024*1024*1024));
+// console.log("OS Total Memory: ",os.freemem()/(1024*1024*1024));
+// console.log("Host Name: ", os.hostname());
+// console.log("Platform Name: ", os.platform());
+console.log("User Name: ", os.userInfo());
+
+// 07:40:31 Video 44 Time End
