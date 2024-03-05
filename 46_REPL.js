e882bcc820400f113b331017420e062821c4eb93
deep-coder1 <ndktbd@gmail.com>
2024-03-05 23:44:03 +0530

    REPL in Node JS, Express JS Ex 46 Add

---
+++ 46_REPL.js
@@ -0,0 +1,44 @@
+// REPL   Vid - 46
+// Read-Eval-Print-Loop
+
+// What is REPL
+// Example of REPL
+// Interview Question
+
+// cmd:
+// E:\Study\Node Tut\Node-JS-Tutorial-CSBS>node
+// Welcome to Node.js v20.11.0.
+// Type ".help" for more information.
+// > 2+2
+// 4
+// > a=3;
+// 3
+// > a=3; b=5; a+b
+// 8
+// > function test(){
+// ... console.log("Test Fun");
+// ... }
+// undefined
+// > test();
+// Test Fun
+// undefined
+// > .editor
+// // Entering editor mode (Ctrl+D to finish, Ctrl+C to cancel)
+// function apple(){
+// console.log("This is a fruit");
+// }
+// --> For back CTRL + D
+
+// > .help
+// .break    Sometimes you get stuck, this gets you out
+// .clear    Alias for .break
+// .editor   Enter editor mode
+// .exit     Exit the REPL
+// .help     Print this help message
+// .load     Load JS from a file into the REPL session
+// .save     Save all evaluated commands in this REPL session to a file
+
+// Press Ctrl+C to abort current expression, Ctrl+D to exit the REPL
+// >
+
+// 07:55:05 Video Time End
